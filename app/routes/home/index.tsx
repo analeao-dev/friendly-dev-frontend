c016d1471cbefc701a83b08bce962830520ad094
Ana Paula Leão <anaqueiroz.dev@gmail.com>
2025-12-22 22:54:12 -0300

    feat: adiciona section últimas postagens a home

--- app/routes/home/index.tsx
+++ app/routes/home/index.tsx
@@ -1,31 +1,45 @@
-import Hero from "~/components/hero";
-import type { Route } from "./+types/index";
-import FeaturedProjects from "~/components/featured-projects";
-import type { Project } from "~/types";
-import AboutPreview from "~/components/about-preview";
+import Hero from '~/components/hero';
+import type { Route } from './+types/index';
+import FeaturedProjects from '~/components/featured-projects';
+import type { PostMeta, Project } from '~/types';
+import AboutPreview from '~/components/about-preview';
+import LatestPosts from '~/components/latest-posts';
 
-export function meta({ }: Route.MetaArgs) {
-  return [
-    { title: "The Friendly Dev | Welcome" },
-    { name: "description", content: "Custom website development" },
-  ];
+export function meta({}: Route.MetaArgs) {
+	return [
+		{ title: 'The Friendly Dev | Welcome' },
+		{ name: 'description', content: 'Custom website development' },
+	];
 }
 
-export async function loader({ request }: Route.LoaderArgs): Promise<{ projects: Project[] }> {
-  const response = await fetch(`${import.meta.env.VITE_URL_API}/projects`);
-  const data = await response.json();
+export async function loader({
+	request,
+}: Route.LoaderArgs): Promise<{ projects: Project[]; posts: PostMeta[] }> {
+	const url = new URL(request.url);
 
-  return { projects: data };
+	const [projectsResponse, postsResponse] = await Promise.all([
+		fetch(`${import.meta.env.VITE_URL_API}/projects`),
+		fetch(new URL('/posts-meta.json', url)),
+	]);
+
+	if (!projectsResponse.ok || !postsResponse.ok) {
+		throw new Error('Failed to fetch projects or posts');
+	}
+
+	const [projects, posts] = await Promise.all([projectsResponse.json(), postsResponse.json()]);
+
+	return { projects: projects, posts: posts };
 }
 
 const HomePage = ({ loaderData }: Route.ComponentProps) => {
-  const { projects } = loaderData;
-  return (
-    <>
-      <FeaturedProjects projects={projects} count={2} />
-      <AboutPreview />
-    </>
-  );
-}
+	const { projects, posts } = loaderData;
+	return (
+		<>
+			<FeaturedProjects projects={projects} count={2} />
+			<AboutPreview />
+			<LatestPosts posts={posts} />
+		</>
+	);
+};
 
 export default HomePage;
